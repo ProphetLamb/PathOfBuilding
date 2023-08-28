3e3d0051297261634ca827e3703f3825ec591d2d
ProphetLamb <prophet.lamb@gmail.com>
2023-08-28 23:42:21 +0200

    Overhaul quicksim

--- tests/skill_rota_worker.py
+++ tests/skill_rota_worker.py
@@ -5,6 +5,7 @@ import math as m
 import typing as t
 import pandas as pd
 from psutil import cpu_count
+import copy
 
 # utility
 
@@ -60,7 +61,11 @@ def round_up_div(x,y) -> int:
 
 def floor(x, base=1):
   return base * m.floor(x/base)
-
+def to_ticks(time: float) -> int:
+    return int(time * 1000)
+def to_time(ticks: int) -> float:
+  return ticks / 1000
+  
 def simulate(data: SkillSetDef) -> t.List[float]:
   time_max = 100
   time_delta = 1 / 10000
@@ -99,117 +104,73 @@ def simulate(data: SkillSetDef) -> t.List[float]:
   times = [time / skill.trigger_count for skill in data.skills]
   return times
 
+def data_to_ticks(data: SkillSetDef):
+  data.akt = to_ticks(data.akt)
+  data.cdt = to_ticks(data.cdt)
+  data.stt = to_ticks(data.stt)
+  for skill in data.skills:
+    skill.cdt = to_ticks(skill.cdt)
+
+def data_to_time(data: SkillSetDef):
+  data.akt = to_time(data.akt)
+  data.cdt = to_time(data.cdt)
+  data.stt = to_time(data.stt)
+  for skill in data.skills:
+    skill.cdt = to_time(skill.cdt)
+
+@dataclass
+class SimState:
+  time: int
+  proposed_trigger_skill_index: int
+  trigger_next: t.List[int]
+  trigger_count: t.List[int]
+
+
 def quick_sim(data: SkillSetDef) -> t.List[float]:
-  class Activation:
-    """ Represents an activation of a skill. """
-    def __init__(self, skill: Skill):
-      self.skill = skill
-      self.delta_time = 0.0
-      self.time = 0.0
-      self.count = 0
+  data_to_ticks(data)
+  state = SimState(0, 0, [0] * len(data.skills), [0] * len(data.skills))
 
-    def __eq__(self, __o: object) -> bool:
-      if isinstance(__o, Activation):
-        return self.skill == __o.skill and self.delta_time == __o.delta_time
-      return False
+  def next_proposed_trigger_skill():
+    """ performs one skill rotation checking for the skill with the minimum trigger time
+    @return: the index of the skill with the minimum trigger time
+    """
+    initial_proposed_trigger_skill_index = state.proposed_trigger_skill_index
+    proposed_trigger_skill_index = (initial_proposed_trigger_skill_index + 1) % len(data.skills)
+    attack_activations_skipped = 1
+    next_trigger_skill_index = None
+    next_trigger_skill_time = None
+    while next_trigger_skill_time is None or proposed_trigger_skill_index != initial_proposed_trigger_skill_index:
+      trigger_activation_time = state.trigger_next[proposed_trigger_skill_index]
+      # the skill can next trigger with an attack
+      trigger_activation_time = int(ceil(trigger_activation_time, data.akt))
+      # the skill is delayed by the number of skipped attack activations
+      trigger_activation_time += attack_activations_skipped * data.akt
+      # the skill can only be triggered after the global cooldown
+      time_to_trigger = trigger_activation_time - state.time
+      time_missing_to_global_trigger = data.cdt - time_to_trigger
+      # update the lowest next trigger time
+      if time_missing_to_global_trigger <= 0 and (next_trigger_skill_time is None or trigger_activation_time < next_trigger_skill_time):
+        next_trigger_skill_time = trigger_activation_time
+        next_trigger_skill_index = proposed_trigger_skill_index
+      attack_activations_skipped += 1
 
-    def get_avg_time(self) -> float:
-      """ returns the average time between activations """
-      return self.time / self.count
+    state.proposed_trigger_skill_index =  next_trigger_skill_index
+    state.time = next_trigger_skill_time
 
-    def time_ready(self) -> float:
-      """ returns the time when the skill is ready """
-      return self.time + self.skill.cdt
+  def activate_proposed_skill():
+    """ activates the proposed skill
+    @return: the time at which the skill was activated
+    """
+    # the skill can next trigger at the activation time + its cooldown
+    state.trigger_next[state.proposed_trigger_skill_index] = state.time + data.skills[state.proposed_trigger_skill_index].cdt
+    state.trigger_count[state.proposed_trigger_skill_index] += 1
 
-    def activate(self, time: float):
-      """ activate the skill at the given time, update the activation """
-      self.delta_time = time - self.time
-      self.time = time
-      self.count += 1
+  while any(cnt <= 1 for cnt in state.trigger_count):
+    activate_proposed_skill()
+    next_proposed_trigger_skill()
 
-  class State:
-    """ Iterator representing the state of a triggered skill rotation. """
-    def __init__(self, skills: t.List[Skill]):
-      self.activations = [Activation(skill) for skill in skills]
-      self.time = 0.0
-      self.current_activation = 0
-
-    def iter(self) -> t.Iterator[Activation]:
-      """ iterate over all activations in order """
-      idx = self.current_activation
-      count = len(self.activations)
-      for _ in range(count):
-        yield self.activations[idx]
-        idx = (idx + 1) % count
-
-    def iter_time_ready(self) -> t.Iterator[t.Tuple[float, Activation]]:
-      """ iterate over all activations and the time at which each skill is ready """
-      # the time at which the next attack will be ready
-      time_penalty = self.time + data.akt
-      for activation in self.iter():
-        # the time until the skill is ready
-        time_ready = activation.time_ready()
-        # wait for the next attack
-        time_ready = ceil(time_ready, data.akt)
-        # wait until the attack rotation is ready
-        time_ready = max(time_ready, time_penalty)
-        yield time_ready, activation
-
-    def get_nearest_ready(self) -> t.Tuple[float, Activation]:
-      """ Returns the next activation and the time until the skill is ready."""
-      nearest_time = 0.0
-      nearest_activation = None
-      for time_ready,activation in self.iter_time_ready():
-        if nearest_activation is None or time_ready < nearest_time:
-          nearest_time = time_ready
-          nearest_activation = activation
-      return nearest_time, nearest_activation
-
-    def activate(self) -> Activation:
-      """ Activates the activation nearest to ready."""
-      time, activation = self.get_nearest_ready()
-      # round up time to the next server tick
-      time = ceil(time, data.stt)
-      activation.activate(time)
-      self.time = time
-      self.current_activation = self.activations.index(activation)
-      return activation
-
-    def skip(self):
-      """ Skips one skill in the rotation. """
-      self.current_activation = (self.current_activation + 1) % len(self.activations)
-
-    def move_next_round(self):
-      """ Move to the next round of activations."""
-      initial_activation = self.current_activation
-      is_initial = True
-      # next until a full round has been completed
-      while self.activate() is not None and (is_initial or self.current_activation != initial_activation):
-        self.skip()
-        is_initial = False
-
-    def get_avg_time(self) -> t.Iterator[float]:
-      """ Returns the average cooldown time for each skill."""
-      for activation in self.activations:
-        yield activation.get_avg_time()
-
-    def limit_simulation(self, count: float):
-      """ Simulate the rotation until the limit is exceeded but ensure that all skills are triggered at least once."""
-      while count > 0 or any(activation.count == 0 for activation in self.iter()):
-        self.move_next_round()
-        count -= 1
-
-  times = [0.0] * len(data.skills)
-  for i in range(len(data.skills)):
-    state = State(data.skills)
-    state.current_activation = i
-    # initial rotation
-    state.move_next_round()
-    # simulate rotations until the limit is exceeded
-    state.limit_simulation(2)
-    times = [time + avg for time,avg in zip(times, state.get_avg_time())]
-
-  return [t / len(data.skills) for t in times]
+  data_to_time(data)
+  return list(to_time(state.time / cnt) for cnt in state.trigger_count)
 
 def calculate(data: SkillSetDef) -> t.List[float]:
   # for the breaking points we visualize everything as log(x) to log(y).
@@ -249,14 +210,14 @@ def plot_skills(skills: t.List[Skill], rates: t.List[float], cdt: float = None):
   # with Pool(cpu_count(logical=False)) as p:
   #   res = list(p.map(exec, data))
   res = list(map(exec, data))
-  calc = [1/t for akt in res for t in akt[1]]
-  sim = [1/t for akt in res for t in akt[0]]
+  calc = [1/t for akt in res for t in akt[0]]
+  sim = [1/t for akt in res for t in akt[1]]
   plot_data(calc, rates, skills, "Calculated")
   plot_data(sim, rates, skills, "Simulated")
 
 def exec(data: SkillSetDef) -> t.Tuple[t.List[float],t.List[float]]:
   for s in data.skills:
     s.reset()
-  calc = calculate(data)
-  sim = simulate(data)
+  calc = calculate(copy.deepcopy(data))
+  sim = simulate(copy.deepcopy(data))
   return calc, sim
